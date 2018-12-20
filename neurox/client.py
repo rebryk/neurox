b54b5e49ef6b071848d1bf454b66361c026c314b
Yurii Rebryk <y.a.rebryk@gmail.com>
2018-12-20 21:05:30 +0300

    Update neuromation client

--- neurox/client.py
+++ neurox/client.py
@@ -1,43 +1,27 @@
+import asyncio
 import subprocess
 from collections import namedtuple
-from functools import partial
-from typing import List
+from functools import wraps
+from typing import List, Any, Callable
 
-import neuromation.cli.command_handlers as command_handlers
 from neuromation.cli.rc import ConfigFactory
-from neuromation.client.jobs import Job, JobDescription
+from neuromation.cli.ssh_utils import remote_debug
+from neuromation.clientv2 import ClientV2
+from neuromation.clientv2.jobs import JobDescription
 
-from neurox.utils import make_executable
+from neurox.ssh_utils import connect_ssh
 
 StatusUpdate = namedtuple('StatusUpdate', ['job_id', 'status'])
 NewJobUpdate = namedtuple('NewJobUpdate', ['job_id', 'status'])
 
 
-class JobHandlerOperations(command_handlers.JobHandlerOperations):
-    def __init__(self, principal: str, tmp_file: str = None):
-        super().__init__(principal)
-        self.tmp_file = tmp_file
+def sync_wait(future: Callable) -> Callable:
+    @wraps(future)
+    def wrapper(*args, **kwargs) -> Any:
+        loop = asyncio.get_event_loop()
+        return loop.run_until_complete(future(*args, **kwargs))
 
-    def start_ssh(self,
-                  job_id: str,
-                  jump_host: str,
-                  jump_user: str,
-                  jump_key: str,
-                  container_user: str,
-                  container_key: str):
-        proxy_command = f'\"ProxyCommand=ssh -i {jump_key} {jump_user}@{jump_host} nc {job_id} 22\"'
-        command = f'ssh -o {proxy_command} -i {container_key} {container_user}@{job_id}'
-
-        try:
-            with open(self.tmp_file, 'w') as file:
-                file.write(command)
-
-            make_executable(self.tmp_file)
-            subprocess.call(args=['open', self.tmp_file])
-        except subprocess.CalledProcessError as e:
-            pass
-
-        return None
+    return wrapper
 
 
 class NeuroxClient:
@@ -76,7 +60,8 @@ class NeuroxClient:
         return list(filter(lambda it: it.status in ['pending', 'running'], jobs))
 
     @staticmethod
-    def job_kill(job_id: str):
+    @sync_wait
+    async def job_kill(job_id: str):
         config = ConfigFactory.load()
 
         if not config.url:
@@ -85,11 +70,12 @@ class NeuroxClient:
         if not config.auth:
             raise ValueError('Specify API token! See for more info `neuro config`.')
 
-        with Job(config.url, config.auth) as job:
-            job.kill(job_id)
+        async with ClientV2(config.url, config.auth) as api:
+            await api.jobs.kill(job_id)
 
     @staticmethod
-    def job_list() -> List[JobDescription]:
+    @sync_wait
+    async def job_list() -> List[JobDescription]:
         config = ConfigFactory.load()
 
         if not config.url:
@@ -98,24 +84,28 @@ class NeuroxClient:
         if not config.auth:
             raise ValueError('Specify API token! See for more info `neuro config`.')
 
-        with Job(config.url, config.auth) as job:
-            return job.list()
+        async with ClientV2(config.url, config.auth) as api:
+            return await api.jobs.list()
 
     @staticmethod
-    def connect_ssh(job_id: str, tmp_file: str):
+    @sync_wait
+    async def connect_ssh(job_id: str, tmp_file: str):
         config = ConfigFactory.load()
         rsa_path = config.github_rsa_path
-        user_name = config.get_platform_user_name()
-        jobs = partial(Job, config.url, config.auth)
-        JobHandlerOperations(user_name, tmp_file).connect_ssh(job_id, rsa_path, 'root', rsa_path, jobs)
+        username = config.get_platform_user_name()
+
+        async with ClientV2(config.url, config.auth) as client:
+            await connect_ssh(client, username, job_id, rsa_path, 'root', rsa_path, tmp_file)
 
     @staticmethod
-    def remote_debug(job_id: str, local_port: int):
+    @sync_wait
+    async def remote_debug(job_id: str, local_port: int):
         config = ConfigFactory.load()
         rsa_path = config.github_rsa_path
-        user_name = config.get_platform_user_name()
-        jobs = partial(Job, config.url, config.auth)
-        JobHandlerOperations(user_name).python_remote_debug(job_id, rsa_path, local_port, jobs)
+        username = config.get_platform_user_name()
+
+        async with ClientV2(config.url, config.auth) as client:
+            await remote_debug(client, username, job_id, rsa_path, local_port)
 
     @staticmethod
     def submit_raw(params: str):
---
+++ neurox/ssh_utils.py
@@ -0,0 +1,55 @@
+import asyncio
+import subprocess
+
+import aiofiles as aiof
+import aiohttp
+from neuromation.cli.ssh_utils import _validate_args_for_ssh_session
+from neuromation.cli.ssh_utils import _validate_job_status_for_ssh_session
+from neuromation.clientv2 import ClientV2
+from neuromation.clientv2.jobs import JobDescription
+
+from neurox.utils import make_executable
+
+
+async def _connect_ssh(
+        username: str,
+        job_status: JobDescription,
+        jump_host_key: str,
+        container_user: str,
+        container_key: str,
+        tmp_file: str):
+    _validate_job_status_for_ssh_session(job_status)
+    # We shall make an attempt to connect only in case it has SSH
+    ssh_hostname = job_status.jump_host()
+    if not ssh_hostname:
+        raise RuntimeError("Job has no SSH server enabled")
+
+    proxy_command = f'\"ProxyCommand=ssh -i {jump_host_key} {username}@{ssh_hostname} nc {job_status.id} 22\"'
+    command = f'ssh -o {proxy_command} -i {container_key} {container_user}@{job_status.id}'
+
+    try:
+        async with aiof.open(tmp_file, 'w') as file:
+            await file.write(command)
+
+        make_executable(tmp_file)
+        proc = await asyncio.create_subprocess_exec('open', tmp_file)
+        await proc.wait()
+    except subprocess.CalledProcessError as e:
+        pass
+
+
+async def connect_ssh(
+        client: ClientV2,
+        username: str,
+        job_id: str,
+        jump_host_key: str,
+        container_user: str,
+        container_key: str,
+        tmp_file: str):
+    _validate_args_for_ssh_session(container_user, container_key, jump_host_key)
+    # Check if job is running
+    try:
+        job_status = await client.jobs.status(job_id)
+    except aiohttp.ClientError as e:
+        raise ValueError(f"Job not found. Job Id = {job_id}") from e
+    await _connect_ssh(username, job_status, jump_host_key, container_user, container_key, tmp_file)
